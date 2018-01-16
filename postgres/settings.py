b336af3a31f37864f7c641c18e55160f725fd664
motherlover <jariblom484@gmail.com>
2018-01-16 13:01:37 +0100

    DB settings change

--- postgres/settings.py
+++ postgres/settings.py
@@ -78,20 +78,21 @@ WSGI_APPLICATION = 'postgres.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'development.sqlite3'),
     }
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'scenario_planning',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres2',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+
+#DATABASES = {
+# #   'default': {
+#  #      'ENGINE': #'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'scenario_planning',
+#        'USER': 'postgres',
+#        'PASSWORD': 'postgres2',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+#}
 
 
 # Password validation
